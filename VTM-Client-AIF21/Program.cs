4356c0e651791a0bee4ca31de09bae6367fd003c
schredlu <schredlu@E211-23-012>
2026-03-06 11:01:19 +0100

    programm an MVC angepasst zuweisung für view und controller fehlen noch

--- VTM-Client-AIF21/Program.cs
+++ VTM-Client-AIF21/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Instrumentation;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,12 +10,25 @@ namespace VTM_Client_AIF21
 {
     internal static class Program
     {
+        private IModel model;
+        private IView view;
+        private IController controller; 
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            // MVC initialisieren
+            model = new Model();
+            view = new Form1();
+            controller = new Controller();
+
+            model.View=view;
+            model.Controller=controller;
+            
+
+            Application.Run(view);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
